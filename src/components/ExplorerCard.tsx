efbc4f5a29221facd02dd73201057c3276441fa0
Luis Cossío <luis.cossio@outlook.com>
2022-06-14 12:49:20 -0500

    updated card to show metrics

--- src/components/ExplorerCard.tsx
+++ src/components/ExplorerCard.tsx
@@ -4,23 +4,32 @@ interface Props {
   title: string;
   author: string;
   authorLink: string;
-  description: string;
+  summary: string;
   projectLink: string;
+  amtRaised: number;
+  numContributors: number;
 }
 
 export const ExplorerCard: FC<Props> = ({
   title,
   author,
   authorLink,
-  description,
+  summary,
   projectLink,
+  amtRaised,
+  numContributors,
 }) => {
+  const roundedAmtRaised = Math.round(amtRaised);
   return (
     <>
       <div className='card w-96 bg-base-100 shadow-xl'>
         <a href={projectLink}>
-          <figure>
+          <figure className='relative'>
+            <div className='w-full h-full bg-slate-200 absolute transition-opacity opacity-0 hover:opacity-80 flex'>
+              <button className='btn btn-primary m-auto'>Donate</button>
+            </div>
             <img
+              className='w-full'
               src='https://api.lorem.space/image/shoes?w=400&h=225'
               alt='Shoes'
             />
@@ -28,33 +37,25 @@ export const ExplorerCard: FC<Props> = ({
         </a>
         <div className='card-body'>
           <a href={projectLink}>
-            <h2 id='title' className='card-title mb-1'>
+            <h2 id='title' className='card-title mb-1 font-mono'>
               {title}
             </h2>
           </a>
-          <p id='author' className='text-xs mb-3'>
+          <p id='author' className='text-xs mb-3 font-mono'>
             By{" "}
             <a className='underline underline-offset-4' href={authorLink}>
               {author}
             </a>
           </p>
-          <p id='description' className='line-clamp-3'>
-            {description}
+          <p id='description' className='line-clamp-3 text-slate-200'>
+            {summary}
           </p>
           <div className='card-actions'>
-            <div className='grid grid-cols-4 gap-2'>
-              <div className='col-span-3'>
-                <label className='input-group'>
-                  <input
-                    type='text'
-                    placeholder='0.01'
-                    className='input input-bordered w-3/4'
-                  />
-                  <span>SOL</span>
-                </label>
-              </div>
-              <button className='btn btn-primary col-span-1'>Donate</button>
-            </div>
+            <p className='font-mono mx-auto text-sm text-center'>
+              <p className='text-xl text-center'>
+                ${roundedAmtRaised}
+              </p>
+              Raised from {numContributors} supporters</p>
           </div>
         </div>
       </div>
@@ -62,12 +63,14 @@ export const ExplorerCard: FC<Props> = ({
   );
 };
 
-const _exampleCard = (
+export const _exampleCard = (
   <ExplorerCard
     title='Minter Project'
     author='minter.sol'
     authorLink='https://minter.sol'
-    description='Make minting process easier with this framework and then do a lot of subsequent lines until we reach more than 3 lines to test for line clamping'
+    summary='Make minting process easier with this framework and then do a lot of subsequent lines until we reach more than 3 lines to test for line clamping'
     projectLink='https://solanagrants.com/minter-project'
+    amtRaised={3012.892}
+    numContributors={76}
   />
 );
