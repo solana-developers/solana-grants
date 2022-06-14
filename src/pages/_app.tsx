4f47ebe240281f74da3eba853aa5d5d4d45f230e
RishiDesai17 <rishindesai@yahoo.com>
2022-06-14 20:59:34 +0530

    navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 import { FC } from 'react';
 import { ContextProvider } from '../contexts/ContextProvider';
 import { AppBar } from '../components/AppBar';
 import { ContentContainer } from '../components/ContentContainer';
 import { Footer } from '../components/Footer';
-import Notifications from '../components/Notification'
+import Notifications from '../components/Notification';
 
 require('@solana/wallet-adapter-react-ui/styles.css');
 require('../styles/globals.css');
@@ -13,18 +12,18 @@ require('../styles/globals.css');
 const App: FC<AppProps> = ({ Component, pageProps }) => {
     return (
         <>
-          <Head>
-            <title>Solana Scaffold Lite</title>
-          </Head>
-
           <ContextProvider>
             <div className="flex flex-col h-screen">
               <Notifications />
-              <AppBar/>
+              <div className="relative mb-20">
+                <div className='absolute left-0 top-0 background-graphic-upper'></div>
+                <div className='absolute left-0 right-0'>
+                  <AppBar/>
+                </div>
+              </div>
               <ContentContainer>
                 <Component {...pageProps} />
               </ContentContainer>
-              <Footer/>
             </div>
           </ContextProvider>
         </>
