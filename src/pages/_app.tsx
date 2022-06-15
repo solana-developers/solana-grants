96b33e569babd283faa9ab8ee6c1e9b8082b2261
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-06-15 22:31:56 +0530

    removed unncessary quotation mark changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,22 @@
-import { AppProps } from "next/app";
-import { FC } from "react";
-import { ContextProvider } from "../contexts/ContextProvider";
-import { AppBar } from "../components/AppBar";
-import { Footer } from "../components/Footer";
-import Notifications from "../components/Notification";
+import { AppProps } from 'next/app';
+import { FC } from 'react';
+import { ContextProvider } from '../contexts/ContextProvider';
+import { AppBar } from '../components/AppBar';
+import { Footer } from '../components/Footer';
+import Notifications from '../components/Notification';
 
-require("@solana/wallet-adapter-react-ui/styles.css");
-require("../styles/globals.css");
+require('@solana/wallet-adapter-react-ui/styles.css');
+require('../styles/globals.css');
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <ContextProvider>
-        <div className="flex flex-col h-screen">
+        <div className='flex flex-col h-screen'>
           <Notifications />
-          <div className="relative mb-20">
-            <div className="absolute left-0 top-0 background-graphic-upper"></div>
-            <div className="absolute left-0 right-0">
+          <div className='relative mb-20'>
+            <div className='absolute left-0 top-0 background-graphic-upper'></div>
+            <div className='absolute left-0 right-0'>
               <AppBar />
             </div>
           </div>
