54536054a032d79cffec559229596b482c41de43
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-06-20 19:21:36 +0530

    made footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ export const Footer: FC = () => {
 
   return (
     <div className="">
-      <footer className="text-center h-[190px] relative lg:text-left">
+      <footer className="text-center xl:h-[190px] relative lg:text-left">
         {/* <div className="lg:overflow-hidden md:overflow-visible h-[260px] absolute right-0 bottom-0">
           <svg
             width="1600"
@@ -44,12 +44,13 @@ export const Footer: FC = () => {
             </defs>
           </svg>
         </div> */}
-        <div className="mx-6 px-20 -my-6 text-center absolute top-0 md:text-left">
+        <div className="mx-6 px-20 md:px-10 xl:px-15 -my-6 text-center absolute top-0 md:text-left">
           <div className="grid lg:grid-cols-5 place-items-center md:grid-cols-2 gap-4">
             <div className="">
               <h6 className="uppercase text-thistle font-montserrat font-bold mb-4 flex items-center justify-center md:justify-start">
                 <div className="mr-2">
                   <svg
+                    className="w-[80%] xl:w-[100%]"
                     width="100%"
                     height="30"
                     viewBox="0 0 101 88"
@@ -79,19 +80,19 @@ export const Footer: FC = () => {
                     </defs>
                   </svg>
                 </div>
-                <p className="text-white font-bold cursor-default font-quantico text-3xl ">
+                <p className="text-white font-bold cursor-default font-quantico text-xl xl:text-3xl ">
                   GRANTS
                 </p>
               </h6>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Consequatur consequuntur dolorum error impedit ipsum porro qui
-                ut. Animi beatae blanditiis earum iusto labore neque non
+              <p className="text-sm xl:text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. -
+                Consequatur consequuntur dolorum error impedit ipsum porro qui -
+                ut. Animi beatae blanditiis earum iusto labore neque non -
                 quibusdam quisquam, quod similique vero.
               </p>
             </div>
-            <div className="">
-              <h6 className="uppercase text-thistle font-montserrat font-bold mb-4 flex justify-center md:justify-start">
+            <div className="text-sm xl:text-base">
+              <h6 className="uppercase text-thistle font-montserrat font-bold mb-4 flex justify-center md:justify-start text-base">
                 Community
               </h6>
               <p className="mb-4">
@@ -115,8 +116,8 @@ export const Footer: FC = () => {
                 </a>
               </p>
             </div>
-            <div className="">
-              <h6 className="uppercase text-thistle font-montserrat font-bold mb-4 flex justify-center md:justify-start">
+            <div className="text-sm xl:text-base">
+              <h6 className="uppercase text-thistle font-montserrat font-bold mb-4 flex justify-center md:justify-start text-base">
                 Services
               </h6>
               <p className="mb-4">
@@ -140,8 +141,8 @@ export const Footer: FC = () => {
                 </a>
               </p>
             </div>
-            <div className="">
-              <h6 className="uppercase text-thistle font-montserrat font-bold mb-4 flex justify-center md:justify-start">
+            <div className="text-sm xl:text-base">
+              <h6 className="uppercase text-thistle font-montserrat font-bold mb-4 flex justify-center md:justify-start text-base">
                 resources
               </h6>
               <p className="mb-4">
@@ -185,9 +186,9 @@ export const Footer: FC = () => {
                 >
                   <path d="M12.02 0c6.614.011 11.98 5.383 11.98 12 0 6.623-5.376 12-12 12-6.623 0-12-5.377-12-12 0-6.617 5.367-11.989 11.981-12h.039zm3.694 16h-7.427c.639 4.266 2.242 7 3.713 7 1.472 0 3.075-2.734 3.714-7m6.535 0h-5.523c-.426 2.985-1.321 5.402-2.485 6.771 3.669-.76 6.671-3.35 8.008-6.771m-14.974 0h-5.524c1.338 3.421 4.34 6.011 8.009 6.771-1.164-1.369-2.059-3.786-2.485-6.771m-.123-7h-5.736c-.331 1.166-.741 3.389 0 6h5.736c-.188-1.814-.215-3.925 0-6m8.691 0h-7.685c-.195 1.8-.225 3.927 0 6h7.685c.196-1.811.224-3.93 0-6m6.742 0h-5.736c.062.592.308 3.019 0 6h5.736c.741-2.612.331-4.835 0-6m-12.825-7.771c-3.669.76-6.671 3.35-8.009 6.771h5.524c.426-2.985 1.321-5.403 2.485-6.771m5.954 6.771c-.639-4.266-2.242-7-3.714-7-1.471 0-3.074 2.734-3.713 7h7.427zm-1.473-6.771c1.164 1.368 2.059 3.786 2.485 6.771h5.523c-1.337-3.421-4.339-6.011-8.008-6.771" />
                 </svg>
-                English - En
+                <p className="text-sm xl:text-base">English - En</p>
                 <svg
-                  className="w-10 h-4 ml-5"
+                  className="w-8 xl:w-10 h-4 ml-2 xl:ml-5"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
