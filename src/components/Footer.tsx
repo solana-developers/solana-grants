8aae5a9215544beb540f6ecbbc75d797b8cb4141
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-06-20 17:55:45 +0530

    updated footer's length, commented out the background

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,8 +13,8 @@ export const Footer: FC = () => {
 
   return (
     <div className="">
-      <footer className="text-center h-52 relative lg:text-left">
-        <div className="overflow-hidden h-60 absolute right-0 bottom-0">
+      <footer className="text-center h-[190px] relative lg:text-left">
+        {/* <div className="lg:overflow-hidden md:overflow-visible h-[260px] absolute right-0 bottom-0">
           <svg
             width="1600"
             height="633"
@@ -43,8 +43,8 @@ export const Footer: FC = () => {
               </linearGradient>
             </defs>
           </svg>
-        </div>
-        <div className="mx-6 px-20 text-center absolute top-0 md:text-left">
+        </div> */}
+        <div className="mx-6 px-20 -my-6 text-center absolute top-0 md:text-left">
           <div className="grid lg:grid-cols-5 place-items-center md:grid-cols-2 gap-4">
             <div className="">
               <h6 className="uppercase text-thistle font-montserrat font-bold mb-4 flex items-center justify-center md:justify-start">
@@ -180,7 +180,7 @@ export const Footer: FC = () => {
                   height="24"
                   xmlns="http://www.w3.org/2000/svg"
                   fillRule="evenodd"
-                  className="mr-2"
+                  className="mr-2 fill-white"
                   clipRule="evenodd"
                 >
                   <path d="M12.02 0c6.614.011 11.98 5.383 11.98 12 0 6.623-5.376 12-12 12-6.623 0-12-5.377-12-12 0-6.617 5.367-11.989 11.981-12h.039zm3.694 16h-7.427c.639 4.266 2.242 7 3.713 7 1.472 0 3.075-2.734 3.714-7m6.535 0h-5.523c-.426 2.985-1.321 5.402-2.485 6.771 3.669-.76 6.671-3.35 8.008-6.771m-14.974 0h-5.524c1.338 3.421 4.34 6.011 8.009 6.771-1.164-1.369-2.059-3.786-2.485-6.771m-.123-7h-5.736c-.331 1.166-.741 3.389 0 6h5.736c-.188-1.814-.215-3.925 0-6m8.691 0h-7.685c-.195 1.8-.225 3.927 0 6h7.685c.196-1.811.224-3.93 0-6m6.742 0h-5.736c.062.592.308 3.019 0 6h5.736c.741-2.612.331-4.835 0-6m-12.825-7.771c-3.669.76-6.671 3.35-8.009 6.771h5.524c.426-2.985 1.321-5.403 2.485-6.771m5.954 6.771c-.639-4.266-2.242-7-3.714-7-1.471 0-3.074 2.734-3.713 7h7.427zm-1.473-6.771c1.164 1.368 2.059 3.786 2.485 6.771h5.523c-1.337-3.421-4.339-6.011-8.008-6.771" />
@@ -188,7 +188,7 @@ export const Footer: FC = () => {
                 English - En
                 <svg
                   className="w-10 h-4 ml-5"
-                  fill="none    "
+                  fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
