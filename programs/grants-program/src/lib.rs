c7f4cc37cef6bea5b09e8676e1901d096fc175f5
RishiDesai17 <rishindesai@yahoo.com>
2022-06-30 05:37:08 +0530

    added schema and data allocation

--- programs/grants-program/src/lib.rs
+++ programs/grants-program/src/lib.rs
@@ -13,3 +13,31 @@ pub mod grants_program {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+
+#[account]
+pub struct GrantInfo {
+    pub total_amount: u32,
+    pub data: String, // This would be an identifier to data stored on our web3 DB
+    pub amount_raised: u32,
+    pub wallet_address: Pubkey,
+    pub total_donors: u32,
+    pub matching_eligible: bool
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const NUMBER_LENGTH: usize = 4;
+const CHAR_LENGTH: usize = 4;
+const DATA_LENGTH: usize = 200 * CHAR_LENGTH; // A string of 200 chars maximum
+const BOOLEAN_LENGTH: usize = 1;
+
+impl GrantInfo {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + NUMBER_LENGTH // total_amount
+        + DATA_LENGTH // data
+        + NUMBER_LENGTH // amount_raised
+        + PUBLIC_KEY_LENGTH // wallet_address.
+        + NUMBER_LENGTH // total_donors
+        + BOOLEAN_LENGTH; // matching_eligible
+}
