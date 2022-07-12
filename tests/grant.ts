efb5705ec0bce2a3e70ab7f951217140154b699b
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-07-12 19:24:44 +0530

    added test to check if number of grants is updating

--- tests/grant.ts
+++ tests/grant.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import {Program} from "@project-serum/anchor";
 import {GrantsProgram} from "../target/types/grants_program";
-import {Transaction, SystemProgram, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
+import {Transaction, SystemProgram, LAMPORTS_PER_SOL, PublicKey, Keypair} from "@solana/web3.js";
 import {encode} from "@project-serum/anchor/dist/cjs/utils/bytes/utf8";
 import {expect} from "chai";
 
@@ -13,6 +13,7 @@ const toBytesInt32 = (num: number): Buffer => {
 };
 
 describe("grant", () => {
+
     // Configure the client to use the local cluster.
     anchor.setProvider(anchor.AnchorProvider.env());
 
@@ -23,7 +24,7 @@ describe("grant", () => {
     let programInfoPDA: PublicKey;
 
     before(async () => {
-        const [newProgramInfoPDA, _bump] = await anchor.web3.PublicKey.findProgramAddress(
+        const [newProgramInfoPDA, program_info_bump] = await anchor.web3.PublicKey.findProgramAddress(
             [
                 encode("program_info"),
             ],
@@ -32,6 +33,34 @@ describe("grant", () => {
 
         programInfoPDA = newProgramInfoPDA;
     })
+
+    async function createGrant(author: Keypair){
+
+        const grantsCount = (await program.account.grantsProgramInfo.fetch(programInfoPDA)).grantsCount;
+
+        const [grantPDA, grant_bump] = await anchor.web3.PublicKey.findProgramAddress(
+            [
+                encode("grant"),
+                toBytesInt32(grantsCount),
+            ],
+            program.programId
+        );
+
+        const targetLamports = LAMPORTS_PER_SOL;
+        const dueDate = 123124;
+
+        await program.methods
+            .createGrant(targetLamports, dueDate)
+            .accounts({
+                grant: grantPDA,
+                programInfo: programInfoPDA,
+                author: author.publicKey,
+            })
+            .signers([author])
+            .rpc();
+
+        return program.account.grant.fetch(grantPDA);
+    }
 
     async function generateFundedKeypair(): Promise<anchor.web3.Keypair> {
         const newKeypair = anchor.web3.Keypair.generate();
@@ -52,8 +81,8 @@ describe("grant", () => {
     }
 
     it("Initializes Grant Program Info!", async () => {
-        // Add your test here.
-        const _tx = await program.methods
+
+        await program.methods
             .initializeProgramInfo()
             .accounts({
                 admin: programWallet.publicKey,
@@ -67,33 +96,15 @@ describe("grant", () => {
 
     });
 
+
     it("Creates a grant", async () => {
 
         const author = await generateFundedKeypair();
-        const grantsCount = (await program.account.grantsProgramInfo.fetch(programInfoPDA)).grantsCount;
-
-        const [grantPDA, _bump] = await anchor.web3.PublicKey.findProgramAddress(
-            [
-                encode("grant"),
-                toBytesInt32(grantsCount),
-            ],
-            program.programId
-        );
 
         const targetLamports = LAMPORTS_PER_SOL;
         const dueDate = 123124;
 
-        const _tx = await program.methods
-            .createGrant(targetLamports, dueDate)
-            .accounts({
-                grant: grantPDA,
-                programInfo: programInfoPDA,
-                author: author.publicKey,
-            })
-            .signers([author])
-            .rpc();
-
-        const grant = await program.account.grant.fetch(grantPDA);
+        const grant = await createGrant(author);
 
         expect(grant.dueDate).to.eql(dueDate);
         expect(grant.author).to.eql(author.publicKey);
@@ -103,4 +114,18 @@ describe("grant", () => {
         expect(grant.escrowCount).to.eql(0);
         expect(grant.info).to.eql("")
     })
+
+    it("Is updating the number of grants", async () => {
+        const author1 = await generateFundedKeypair();
+        const author2 = await generateFundedKeypair();
+
+
+        await createGrant(author1)
+        await createGrant(author2)
+
+        const grantsCount = (await program.account.grantsProgramInfo.fetch(programInfoPDA)).grantsCount;
+        expect(grantsCount).to.eql(3)
+
+    })
+
 });
