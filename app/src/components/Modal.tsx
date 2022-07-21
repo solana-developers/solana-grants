f2a2e3e05e331ce5586554509208be9d4716f34e
RishiDesai17 <rishindesai@yahoo.com>
2022-07-21 23:17:22 +0530

    modal props

--- app/src/components/Modal.tsx
+++ app/src/components/Modal.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export default function Modal({ setpreview, classNameForModalBoxStyling, children }) {
+type ModalProps = {
+  setpreview: (preview: boolean) => {};
+  classNameForModalBoxStyling?: string;
+  children: any;
+}
+
+export default function Modal({ setpreview, classNameForModalBoxStyling, children }: ModalProps) {
     return (
       <>
         <div className="modal" id="my-modal-2">
-          <div className={`modal-box ${classNameForModalBoxStyling}`}>
+          <div className={`modal-box ${classNameForModalBoxStyling || ""}`}>
             <a href="#my-modal-2" className="btn btn-sm btn-circle absolute right-2 top-2" onClick={() => setpreview(false)}>✕</a>
             {children}
           </div>
