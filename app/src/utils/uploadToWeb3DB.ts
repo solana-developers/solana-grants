22b073f413adfdf0cc3c18f544134175d497420d
RishiDesai17 <rishindesai@yahoo.com>
2022-07-27 01:50:56 +0530

    transaction handling in upload to we3DB util

--- app/src/utils/uploadToWeb3DB.ts
+++ app/src/utils/uploadToWeb3DB.ts
@@ -1,12 +1,12 @@
 import { WebBundlr } from "@bundlr-network/client";
 import { WalletContextState } from "@solana/wallet-adapter-react";
 import { DEVNET_API, BUNDLR_DEVNET } from "../constants";
+import { TransactionDetail } from '../constants/types';
 
-export default async function uploadToWeb3DB(wallet: WalletContextState, data: Object) {
+export default async function uploadToWeb3DB(wallet: WalletContextState, data: Object, setTransactionsList: React.Dispatch<React.SetStateAction<TransactionDetail[]>>) {
   try {
     if (!wallet || !wallet.connected) {
-      alert("Wallet not connected");
-      return { err: true };
+      return { err: true, message: "Wallet not connected" };
     }
 
     await wallet.connect();
@@ -16,6 +16,7 @@ export default async function uploadToWeb3DB(wallet: WalletContextState, data: O
     const bundlr = new WebBundlr(BUNDLR_DEVNET, "solana", provider, { providerUrl: DEVNET_API });
     
     const dataToBeUploaded = JSON.stringify(data);
+    let transactionCount = 0;
   
     const price = await bundlr.getPrice(dataToBeUploaded.length);
     const balance = await bundlr.getLoadedBalance();
@@ -26,13 +27,27 @@ export default async function uploadToWeb3DB(wallet: WalletContextState, data: O
       // We multiply by 1.1 to make sure we don't run out of funds
       await bundlr.fund(price.minus(balance).multipliedBy(1.1).c[0]);
     }
+
+    setTransactionsList((transactionsList) => {
+      const newTransactionsList = [...transactionsList];
+      newTransactionsList[transactionCount].isCompleted = true;
+      return newTransactionsList;
+    });
+    transactionCount += 1;
     
     const tags = [{ name: "Content-Type", value: "text/plain" }];
     const response = await bundlr.uploader.upload(Buffer.from(dataToBeUploaded), tags);
+
+    setTransactionsList((transactionsList) => {
+      const newTransactionsList = [...transactionsList];
+      newTransactionsList[transactionCount].isCompleted = true;
+      return newTransactionsList;
+    });
+    transactionCount += 1;
   
-    return { err: false, id: response.data.id };  
+    return { err: false, id: response.data.id, transactionCount };  
   } catch (error) {
-    return { err: true }
+    return { err: true, message: "Something went wrong!" }
   }
   
 }
