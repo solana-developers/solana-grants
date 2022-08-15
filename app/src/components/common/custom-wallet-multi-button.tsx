7464bf5ea1fa23092248126d915cc6001c49e164
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-08-15 21:04:30 +0530

    updated custom wallet multi button with proper variants, textVariants

--- app/src/components/common/custom-wallet-multi-button.tsx
+++ app/src/components/common/custom-wallet-multi-button.tsx
@@ -5,65 +5,56 @@ import {IconType} from 'react-icons';
 import {WalletDisconnectButton, WalletMultiButton} from "@solana/wallet-adapter-react-ui";
 
 /**
- * Properties for an interactable button component.
+ * props for a button component.
  */
 type ButtonProps = {
     className?: string;
-    reversed?: boolean;
     onClick?: () => void;
-    variant:
-        | 'black'
-        | 'orange'
+    variant?:
         | 'transparent'
-        | 'danger'
-        | 'label';
     text?: string;
+    textColorVariant?:
+        | 'black'
+        | 'white'
     icon?: IconType;
-    children?: React.ReactNode;
-    ref?: React.Ref<HTMLButtonElement>;
-    type?: 'button' | 'submit' | 'reset';
     connected: boolean;
     disabled?: boolean;
 };
 
-/**
- * Pre-defined styling, according to agreed-upon design-system.
- */
 const variants = {
-    black: 'border-transparent bg-black text-white',
-    orange: 'border-transparent bg-blue-magenta text-black', // TODO: Change name of variant to `highlight`.
-    transparent: 'text-white', // TODO: Change name of variant to `outlined`.
-    danger: 'border-secondary text-danger hover:text-white hover:bg-secondary',
-    label: 'text-secondary hover:text-white !p-0 border-none',
+    transparent: 'bg-transparent border-2',
+};
+
+const textColorVariants = {
+    black: 'text-black',
+    white: 'text-white',
 };
 
 /**
- * Definition of an interactable button component.
  *
- * @param type `type` Attribute of the `<button>` element.
  * @param variant Variations relating to pre-defined styling of the element
  * @param text Text to display in the button.
  */
 const CustomWalletMultiButton = ({
                                      className,
-                                     reversed = false,
                                      onClick,
-                                     variant = 'black',
+                                     variant,
+                                     textColorVariant,
                                      text: value,
-                                     children,
                                      icon,
                                      connected = false,
-                                     disabled = false,
+                                     disabled = false
+
                                  }: ButtonProps) => (
     connected ?
         <WalletDisconnectButton
             className={cn(
                 variants[variant],
                 'flex h-11 max-h-full w-fit items-center justify-center gap-3 whitespace-nowrap rounded-full border transition-all',
-                variant !== 'label' &&
+                textColorVariants[textColorVariant],
+                'hover:!text-black',
                 'hover:-translate-y-[0.15rem] active:translate-y-[0.025rem] active:scale-[0.975]',
-                ((icon && (!value && !children)) ? 'aspect-square p-3' : 'px-5 py-3'),
-                reversed && 'flex-row-reverse',
+                (icon && !value) ? 'aspect-square p-3' : 'px-5 py-3',
                 className,
             )}
             onClick={onClick}
@@ -71,31 +62,31 @@ const CustomWalletMultiButton = ({
         >
             {icon && React.createElement(icon, {size: 20})}
             {value && (
-                <Text variant={variant === 'label' ? 'label' : 'input'}>
+                <Text variant='input'>
                     {value}
                 </Text>
             )}
-            {children}
         </WalletDisconnectButton>
         :
-        <WalletMultiButton className={cn(
-            variants[variant],
-            'flex h-11 max-h-full w-fit items-center justify-center gap-3 whitespace-nowrap rounded-full border transition-all',
-            variant !== 'label' &&
-            'hover:-translate-y-[0.15rem] active:translate-y-[0.025rem] active:scale-[0.975]',
-            ((icon && (!value && !children)) ? 'aspect-square p-3' : 'px-5 py-3'),
-            reversed && 'flex-row-reverse',
-            className,
-        )} onClick={onClick}
-                           disabled={disabled}
+        <WalletMultiButton
+            className={cn(
+                variants[variant],
+                'flex h-11 max-h-full w-fit items-center justify-center gap-3 whitespace-nowrap rounded-full border transition-all',
+                textColorVariants[textColorVariant],
+                'hover:!text-black',
+                'hover:-translate-y-[0.15rem] active:translate-y-[0.025rem] active:scale-[0.975]',
+                (icon && !value) ? 'aspect-square p-3' : 'px-5 py-3',
+                className,
+            )}
+            onClick={onClick}
+            disabled={disabled}
         >
             {icon && React.createElement(icon, {size: 20})}
             {value && (
-                <Text variant={variant === 'label' ? 'label' : 'input'}>
+                <Text variant='input'>
                     {value}
                 </Text>
             )}
-            {children}
         </WalletMultiButton>
 );
 
